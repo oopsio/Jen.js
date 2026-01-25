d4e833c6442965969a19c4417ea5fc2775b9fbc7
VSSCO <161714242+kessud2021@users.noreply.github.com>
2026-01-25 21:24:36 +0530

    Create index.tsx

---
+++ src/cli/templates/ssg/site/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My SSG App</h1>
+      <p>This page was generated from the SSG template.</p>
+    </div>
+  );
+}
