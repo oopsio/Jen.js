382ca2ac6fc8c1b57a6d3ba2db325250d1b695a2
VSSCO <161714242+kessud2021@users.noreply.github.com>
2026-01-25 20:33:05 +0530

    Create log.ts

---
+++ src/shared/log.ts
@@ -0,0 +1,15 @@
+function ts() {
+  return new Date().toISOString();
+}
+
+export const log = {
+  info(msg: string) {
+    console.log(`[${ts()}] [INFO] ${msg}`);
+  },
+  warn(msg: string) {
+    console.warn(`[${ts()}] [WARN] ${msg}`);
+  },
+  error(msg: string) {
+    console.error(`[${ts()}] [ERROR] ${msg}`);
+  }
+};
