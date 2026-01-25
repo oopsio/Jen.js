e8b8f8bf76c647811cf52e6038a3b38df0ceb061
VSSCO <161714242+kessud2021@users.noreply.github.com>
2026-01-25 21:26:05 +0530

    Update create.js

--- src/cli/commands/create.js
+++ src/cli/commands/create.js
@@ -5,7 +5,7 @@ export function createCommand(args) {
   const name = args[0];
   const template = args[1]?.split("=")[1] || "ssg";
   if (!name) {
-    console.log("Usage: jen create <name> --template=<ssg|ssr>");
+    console.log("Usage: myfw create <name> --template=<ssg|ssr>");
     return;
   }
 
@@ -24,12 +24,13 @@ export function createCommand(args) {
   console.log(`Created new app '${name}' using ${template.toUpperCase()} template`);
 }
 
+// Recursively copy folder
 function copyFolder(src, dest) {
   fs.readdirSync(src).forEach((file) => {
     const srcPath = path.join(src, file);
     const destPath = path.join(dest, file);
     if (fs.statSync(srcPath).isDirectory()) {
-      fs.mkdirSync(destPath);
+      fs.mkdirSync(destPath, { recursive: true });
       copyFolder(srcPath, destPath);
     } else {
       fs.copyFileSync(srcPath, destPath);
