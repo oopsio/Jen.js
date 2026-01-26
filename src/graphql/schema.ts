9f0edf9245d51351fc9bd3ba413290fc734f0d96
VSSCO <161714242+kessud2021@users.noreply.github.com>
2026-01-26 09:43:59 +0530

    Create schema.ts

---
+++ src/graphql/schema.ts
@@ -0,0 +1,17 @@
+import { buildSchema } from 'graphql';
+
+export const schema = buildSchema(`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
+  type Query {
+    users: [User]
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User
+  }
+`);
