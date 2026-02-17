98490a3b932dc3b2e574d71ff1eb1962b925d0f4
VSSCO <161714242+kessud2021@users.noreply.github.com>
2026-02-17 19:49:01 +0530

    chore(design): update create-app's template design and fix things so it works

---
+++ packages/create-jen-app/templates/static/site/(home).tsx
@@ -0,0 +1,12 @@
+export default function Index() {
+  return (
+    <div className="container">
+      <h1>Jen.js</h1>
+
+      <div className="card">
+        <h2>You're ready 🚀</h2>
+        <p>Edit (home).tsx to start developing!</p>
+      </div>
+    </div>
+  );
+}
