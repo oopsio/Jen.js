21e065cbec7ed529fdd7ba9fc4d26d45b50048ae
VSSCO <161714242+kessud2021@users.noreply.github.com>
2026-03-04 17:10:39 +0530

    chore: remove unwanted scripts

--- fix_imports.ps1
+++
@@ -1,9 +0,0 @@
-$files = Get-ChildItem -Path "g:\jen.js\src" -Filter "*.ts" -Recurse | Where-Object { Select-String -Pattern "@src/" -Path $_.FullName -Quiet }
-
-foreach ($file in $files) {
-  Write-Host "Processing: $($file.FullName)"
-  $content = Get-Content $file.FullName -Raw
-  $content = $content -replace '@src/', '../'
-  Set-Content $file.FullName $content -Encoding UTF8
-  Write-Host "Fixed: $($file.FullName)"
-}
